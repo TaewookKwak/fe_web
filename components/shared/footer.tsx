07ba5c07119af49a2c76c2e4c9fb4562a778af68
TaewookKwak <rhkrxodnr@gmail.com>
2024-01-16 11:45:11 +0900

    style : footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Sam. All Rights reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
