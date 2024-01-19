04aec47a47af1b1e52fae0c6b4f244cba5821069
TaewookKwak <rhkrxodnr@gmail.com>
2024-01-19 14:26:15 +0900

    test

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,11 @@
 "use server";
-import { revalidatePath } from "next/cache";
+import Event from "@/lib/database/models/event.model";
+import Order from "@/lib/database/models/order.model";
+import User from "@/lib/database/models/user.model";
 import { CreateUserParams, UpdateUserParams } from "@/types";
-import { handleError } from "../utils";
+import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database";
-import User from "../database/models/user.model";
-import Order from "../database/models/order.model";
-import Event from "../database/models/event.model";
-import { NextResponse } from "next/server";
+import { handleError } from "@/lib/utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
