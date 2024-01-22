fbc2f74833f8150d0bf8c985fe7b60a65c59dc99
TaewookKwak <rhkrxodnr@gmail.com>
2024-01-22 09:26:16 +0900

    feat : event page 제작

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,24 @@
+import EventForm from "@/components/shared/event-from";
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+const CreateEventPage = () => {
+  const { sessionClaims } = auth(); // 세션 정보를 가져옵니다.
+
+  const userId = sessionClaims?.userId as string; // 세션 정보에서 userId를 가져옵니다.
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Create Event
+        </h3>
+      </section>
+      <div className="wrapper my-8">
+        <EventForm userId={userId} type="Create" />
+      </div>
+    </>
+  );
+};
+
+export default CreateEventPage;
